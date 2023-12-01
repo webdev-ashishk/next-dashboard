f0c7cb8bb07e2771307b1c01d1db46e79e769803
webdev-ashishk <webdev.ashishk@gmail.com>
2023-12-01 10:05:20 +0530

    api working

--- app/page.js
+++ app/page.js
@@ -1,7 +1,22 @@
-export default function Home() {
+async function getData() {
+  const res = await fetch("https://test-next-api-piei.vercel.app/api/users");
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getData();
+  console.log(data);
   return (
-    <div>
+    <div style={{ margin: "10px" }}>
       <h1> @ home page </h1>
+      <h1>List of all users!</h1>
+      {data.map((items) => {
+        return <div key={items.password}>{items.name}</div>;
+      })}
     </div>
   );
 }
