50d713a75a5d43809cb41cdea51217cd241455d8
webdev-ashishk <webdev.ashishk@gmail.com>
2023-12-01 09:48:40 +0530

    initial api commit

---
+++ app/api/users/route.js
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+
+export async function GET(req) {
+  return NextResponse.json([
+    {
+      name: "anjuK",
+      password: "anjuK098",
+    },
+    {
+      name: "avd",
+      password: "avd123",
+    },
+    {
+      name: "deepika",
+      password: "deepika0343",
+    },
+    {
+      name: "ashishK",
+      password: "anjuK098",
+    },
+    {
+      name: "ra",
+      password: "anjuK098",
+    },
+  ]);
+}
---
+++ utils/users.js
@@ -0,0 +1,14 @@
+export const user = [
+  {
+    name: "anjuK",
+    college: "AZAD",
+  },
+  {
+    name: "DEEPIKA",
+    college: "AZAD",
+  },
+  {
+    name: "AVD",
+    college: "LIT",
+  },
+];
