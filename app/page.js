03b0ea0297f319b6a017719f4f87b80f883166d3
webdev-ashishk <webdev.ashishk@gmail.com>
2023-12-05 02:27:03 +0530

    updated with revalidate api data

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
+export const revalidate = 5; // revalidate at most every hour
 async function getData() {
-  const res = await fetch(process.env.API_URL);
+  const res = await fetch(process.env.API_URL, { next: { revalidate: 5 } });
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
 
   return res.json();
 }
-
 export default async function Home() {
   const data = await getData();
   console.log(data);
