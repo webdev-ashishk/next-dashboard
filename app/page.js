e4bffe00fcb2600b2a653ca836c24a014d719371
webdev-ashishk <webdev.ashishk@gmail.com>
2023-12-04 21:31:56 +0530

    updated make some code comment

--- app/page.js
+++ app/page.js
@@ -1,22 +1,22 @@
-async function getData() {
-  const res = await fetch("http://localhost:3000/api/users");
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+// async function getData() {
+//   const res = await fetch("http://localhost:3000/api/users");
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
 
-  return res.json();
-}
+//   return res.json();
+// }
 
 export default async function Home() {
-  const data = await getData();
-  console.log(data);
+  // const data = await getData();
+  // console.log(data);
   return (
     <div style={{ margin: "10px" }}>
       <h1> @ home page </h1>
       <h1>List of all users!</h1>
-      {data.map((items) => {
+      {/* {data.map((items) => {
         return <div key={items.id}>{items.email}</div>;
-      })}
+      })} */}
     </div>
   );
 }
